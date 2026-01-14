56599bc60f754ee855d7f00d39f61e2974bbbe37
Aurélie Martin <aurelie.martin95@hotmail.fr>
2026-01-14 16:17:17 +0100

    Initial commit

---
+++ src/main/java/com/Credit_Based_Resource_Allocator/CreditBasedResourceAllocatorApplication.java
@@ -0,0 +1,13 @@
+package com.Credit_Based_Resource_Allocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditBasedResourceAllocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditBasedResourceAllocatorApplication.class, args);
+	}
+
+}
